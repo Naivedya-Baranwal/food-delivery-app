fb40accb0f6013b5bfb943c1a2de532756edcd4f
Naivedya Baranwal <naivedyab198@gmail.com>
2025-05-11 19:59:40 +0530

    db update

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,5 +2,3 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     await mongoose.connect('mongodb+srv://naivedyab198:4nNSAwe9KAra7d0U@cluster0.ywbfp.mongodb.net/food-del').then(()=>console.log("DB Connected"));
-
-}
