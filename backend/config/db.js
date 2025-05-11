22a7899d5f98c7d0ef1cbf0c37bda2be26774dc3
Naivedya Baranwal <naivedyab198@gmail.com>
2025-05-11 18:30:42 +0530

    Initial commit: Add admin, backend, frontend folders, .gitignore, and .gitattributes

---
+++ backend/config/db.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    await mongoose.connect('mongodb+srv://naivedyab198:4nNSAwe9KAra7d0U@cluster0.ywbfp.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+
+}
