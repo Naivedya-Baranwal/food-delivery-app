37479544d76bacdba2736a8562c8a0646426850e
Naivedya Baranwal <naivedyab198@gmail.com>
2025-08-28 02:32:38 +0530

    added authentication

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { login, logout,verifyAdmin } from "../controllers/adminController.js";
-
+import adminAuthMiddleware from "../middleware/adminAuth.js";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/login",login);
 adminRouter.post("/logout",logout);
-adminRouter.get("/verify", verifyAdmin);
+adminRouter.get("/verify",adminAuthMiddleware, verifyAdmin);
 
 export default adminRouter;
