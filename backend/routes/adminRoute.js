f9e87e349d563eaeeda0a0ad6a8160ab081f6876
Naivedya Baranwal <naivedyab198@gmail.com>
2025-08-28 02:41:56 +0530

    updated adminauthroute

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,11 +1,10 @@
 import express from "express";
 import { login, logout,verifyAdmin } from "../controllers/adminController.js";
-import adminAuthMiddleware from "../middleware/adminAuth.js";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/login",login);
 adminRouter.post("/logout",logout);
-adminRouter.get("/verify",adminAuthMiddleware, verifyAdmin);
+adminRouter.get("/verify", verifyAdmin);
 
 export default adminRouter;
